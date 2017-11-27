78d44c35b3894fe1e3d34e71c6ac357f27b73df1
Tao Ong <taoong@berkeley.edu>
2017-11-27 11:56:47 -0800

    added minitrees

--- src/components/FriendBlock.js
+++ src/components/FriendBlock.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 
 import Activity from "./Activity";
-import CategoryBlock from "./CategoryBlock";
 import classNames from "classnames";
 import { Scrollbars } from "react-custom-scrollbars";
 
@@ -19,6 +18,10 @@ class FriendBlock extends Component {
     this.handleClick = this.handleClick.bind(this);
   }
 
+
+  
+
+
   setActive() {
     this.props.handleClick(this.props.Index);
   }
@@ -35,28 +38,83 @@ class FriendBlock extends Component {
   }
 
   render() {
+    let testColor = {
+        background: "rgb(20,20,20)",
+        backgroundSize: "cover",
+        height: "150px",
+        width: "50px"
+      }
+    let aweColor = {
+      background: "rgb(" + (235-0*20).toString() + ", " + 
+                           (255-0).toString() + "," + 
+                           (235-0*8).toString(),
+      backgroundSize: "cover",
+      height: "150px",
+      width: "50px"
+    }
+
+  let gratitudeColor = {
+      background: "rgb(" + 255 + ", " + 
+                           (250-0*15) + "," + 
+                           (250-0*15).toString(),
+      backgroundSize: "cover",
+      height: "150px",
+      width: "50px"
+    }
+
+  let kindnessColor = {
+      background: "rgb(" + 255 + ", " + 
+                           255 + "," + 
+                           (250-0*15).toString(),
+      backgroundSize: "cover",
+      height: "150px",
+      width: "50px"
+    }
+
+  let mindfulnessColor = {
+      background: "rgb(" + (245-0*15) + ", " + 
+                           (245-0*15) + "," + 
+                           255,
+      backgroundSize: "cover",
+      height: "150px",
+      width: "50px"
+    }
+
+  let resilienceColor = {
+      background: "rgb(" + 255 + ", " + 
+                           (255-20-0).toString() + "," + 
+                           (235-0*20).toString(),
+      backgroundSize: "cover",
+      height: "150px",
+      width: "50px"
+    }
+
     return (
       <div className="card">
         <div className="card-body">
           <div className="row">
-            <div className="col-lg-3">
+            <div className="col-lg-2">
               <img
                 src={this.props.user.image}
                 className="no-padding"
               />
             </div>
-            <div className="col-lg-5">
+            <div className="col-lg-6">
               <h3 style={{ color: "black" }}> {this.props.user.name}</h3>
             </div>
             <div className="col-lg-4">
-              <img
-                src=""
+              <div className="category--miniimage-container">
+          
+                <div className="category--miniimage" style={aweColor}></div>
+                <div className="category--miniimage" style={gratitudeColor}></div>
+                <div className="category--miniimage" style={kindnessColor}></div>
+                <div className="category--miniimage" style={mindfulnessColor}></div>
+                <div className="category--miniimage" style={resilienceColor}></div>
                 
-              />
+              
+              </div>
             </div>
           </div>
-
-          <hr style={{ marginBottom: "0px" }} />
         </div>
       </div>
     );
