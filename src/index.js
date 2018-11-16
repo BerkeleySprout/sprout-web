271aea6efd72c6c9d1e7a5acad008d7ff6896b4b
Tao Ong <taoong@berkeley.edu>
2018-11-16 13:25:06 -0800

    Fixed npm issues

--- src/index.js
+++ src/index.js
@@ -3,16 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import * as admin from "firebase-admin";
-
-var admin = require("firebase-admin");
-
-var serviceAccount = require("path/to/serviceAccountKey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://<DATABASE_NAME>.firebaseio.com"
-});
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
